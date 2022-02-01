c05873286dbfad066125cf4e8f683d9bbcf2a2fd
Benatovich <sambenatovich@gmail.com>
2022-02-01 18:43:39 -0500

    OrderImages are working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import FoodDropdown from './FoodDropdown';
+import DrinkDropdown from './DrinkDropdown';
+import SideDropdown from './SideDropdown';
+import OrderNameInput from './OrderNameInput';
+import OrderImages from './OrderImages';
+import InstructionsForm from './InstructionsForm';
+import InstructionsList from './InstructionsList';
 
 function App() {
+  const [foodId, setFoodId] = useState(1);
+  const [drinkId, setDrinkId] = useState(1);
+  const [sideId, setSideId] = useState(1);
+  const [orderName, setOrderName] = useState('customer');
+  const [instructions, setInstructions] = useState(['N/A']);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='order'>
+        <h3 className='order-name'>Order for {orderName}</h3>
+        <OrderImages
+          foodId={foodId}
+          drinkId={drinkId}
+          sideId={sideId} />
+      
+      </div>
     </div>
   );
 }
